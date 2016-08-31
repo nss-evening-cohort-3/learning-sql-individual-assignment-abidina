177c432af2ba253006b4507370456f448dd7d792
abbie <abigail.c.holder@gmail.com>
2016-08-30 21:31:11 -0500

    3 & 4

---
+++ 3.sql
@@ -0,0 +1,1 @@
+SELECT Customer.FirstName || " " || Customer.LastName AS 'Customer Name', Country, InvoiceID, InvoiceDate, BillingCountry FROM Invoice JOIN Customer ON Customer.CustomerId == Invoice.CustomerID WHERE Customer.Country IS 'Brazil';
---
+++ 4.sql
@@ -0,0 +1,1 @@
+SELECT Employee.FirstName || " " || Employee.LastName AS 'Employee Name' FROM Employee WHERE Employee.Title IS 'Sales Support Agent';
