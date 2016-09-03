e15d574fa109fd14920836e03f8ca6b1e23506d1
abbie <abigail.c.holder@gmail.com>
2016-09-03 11:13:37 -0500

    fixed 25

--- 25.sql
+++ 25.sql
@@ -5,7 +5,6 @@ SELECT
 FROM Track
 JOIN InvoiceLine ON InvoiceLine.TrackId == Track.TrackId
 JOIN Invoice ON Invoice.InvoiceId == InvoiceLine.InvoiceId
-WHERE InvoiceDate
 GROUP BY Track.Name
 ORDER BY "Quantity" DESC
 LIMIT 5;
