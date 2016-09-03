5597fc89748a19bdd6c551109d5e82c002322709
abbie <abigail.c.holder@gmail.com>
2016-09-03 11:35:51 -0500

    Okay that's better

--- 25.sql
+++ 25.sql
@@ -1,9 +1,10 @@
-SELECT Track.Name AS 'Title', Album.Title AS 'Album Name',
+SELECT Track.Name AS 'Title', Artist.Name AS 'Artist Name', Album.Title AS 'Album Name',
   COUNT(InvoiceLine.Quantity) AS 'Times Purchased'
 FROM Track
 JOIN InvoiceLine ON InvoiceLine.TrackId == Track.TrackId
 JOIN Invoice ON Invoice.InvoiceId == InvoiceLine.InvoiceId
 JOIN Album ON Album.AlbumId == Track.AlbumId
+JOIN Artist ON Artist.ArtistId == Album.ArtistId
 GROUP BY Track.Name
 ORDER BY "Times Purchased" DESC
 LIMIT 5;
