890113a97a2cd06041201af158adafb1941d0104
abbie <abigail.c.holder@gmail.com>
2016-09-03 11:11:54 -0500

    Fixed 24

--- 24.sql
+++ 24.sql
@@ -1,11 +1,11 @@
--- SELECT
---   Track.*,
---   COUNT(InvoiceLine.Quantity) AS 'Quantity',
---   Invoice.InvoiceDate
--- FROM Track
--- JOIN InvoiceLine ON InvoiceLine.TrackId == Track.TrackId
--- JOIN Invoice ON Invoice.InvoiceId == InvoiceLine.InvoiceId
--- WHERE InvoiceDate
--- GROUP BY Track.Name
--- ORDER BY "Quantity" DESC
--- LIMIT 1;
+SELECT
+  Track.*,
+  COUNT(InvoiceLine.Quantity) AS 'Quantity',
+  Invoice.InvoiceDate
+FROM Track
+JOIN InvoiceLine ON InvoiceLine.TrackId == Track.TrackId
+JOIN Invoice ON Invoice.InvoiceId == InvoiceLine.InvoiceId
+WHERE InvoiceDate LIKE "2013%"
+GROUP BY Track.Name
+ORDER BY "Quantity" DESC
+LIMIT 1;
