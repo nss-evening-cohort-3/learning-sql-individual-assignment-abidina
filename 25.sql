a9c1e39c42d7c8b4d83c08b181a1aa410cee64dd
abbie <abigail.c.holder@gmail.com>
2016-09-03 11:34:22 -0500

    oops

--- 25.sql
+++ 25.sql
@@ -1,17 +1,9 @@
-SELECT Track.*,
-  COUNT(InvoiceLine.Quantity) AS 'Times Purchased', Invoice.InvoiceDate
+SELECT Track.Name AS 'Title', Album.Title AS 'Album Name',
+  COUNT(InvoiceLine.Quantity) AS 'Times Purchased'
 FROM Track
 JOIN InvoiceLine ON InvoiceLine.TrackId == Track.TrackId
 JOIN Invoice ON Invoice.InvoiceId == InvoiceLine.InvoiceId
-GROUP BY Track.Name
-ORDER BY "Times Purchased" DESC
-LIMIT 5;
-
-SELECT Track.TrackId AS 'Track ID', Track.Name AS 'Title', Track.AlbumId AS 'Album ID',
-  COUNT(InvoiceLine.Quantity) AS 'Times Purchased', Invoice.InvoiceDate AS 'Invoice Date'
-FROM Track
-JOIN InvoiceLine ON InvoiceLine.TrackId == Track.TrackId
-JOIN Invoice ON Invoice.InvoiceId == InvoiceLine.InvoiceId
+JOIN Album ON Album.AlbumId == Track.AlbumId
 GROUP BY Track.Name
 ORDER BY "Times Purchased" DESC
 LIMIT 5;
